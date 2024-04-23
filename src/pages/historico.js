21661a58a4cef201dfe8ad5ba9db487df642eab6
streeese <94462388+streeese@users.noreply.github.com>
2024-04-22 20:49:44 -0400

    Edit Historico

--- src/pages/historico.js
+++ src/pages/historico.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import Navbar from '../components/navbar'; 
 import Footer from '../components/footer'; 
 import Head from 'next/head';
-import { useCallback } from 'react'; 
 // import HistoricoReservas from '../components/HistoricoReservas'; -- comento esta linea porque al dejarla activa da error en las páginas
 
-const scrollToTop = useCallback(() => {
-  document.getElementById('Inicio').scrollIntoView({ behavior: 'smooth' });
-}, []);
 
 const Historico = () => {
+  const [opcionSeleccionada, setOpcionSeleccionada] = useState(2); // Por defecto, mostrar Historico Reservas
+  
+  const handleClick = (opcion) => {
+    setOpcionSeleccionada(opcion);
+  };
+
+  const scrollToTop = useCallback(() => {
+    document.getElementById('Inicio').scrollIntoView({ behavior: 'smooth' });
+  }, []);
+
   return (
     <div>
       <Head>
@@ -22,15 +28,39 @@ const Historico = () => {
         />
       </Head>
 
-      <Navbar /> {/* Incluye el Navbar */}
-      <div className="container mx-auto px-4 py-8"> {/* Contenedor principal */}
-        <h1 className="text-3xl font-bold mb-4">Historial de Reservas</h1>
-        <div className="bg-white shadow-md rounded-lg p-6"> {/* Estilo del contenedor del componente */}
-          <HistoricoReservas /> {/* Componente de historial de reservas */}
+      <Navbar />
+
+      <main className="max-w-6xl mx-auto">
+        <div className="flex">
+          <div className="sticky w-64 bg-purple-800 text-white">
+            <ul>
+              <li
+                className={`p-4 cursor-pointer ${opcionSeleccionada === 1 ? 'bg-purple-700' : 'hover:bg-purple-700'}`}
+                onClick={() => handleClick(1)}
+              >
+                Mis Datos
+              </li>
+              <li
+                className={`p-4 cursor-pointer ${opcionSeleccionada === 2 ? 'bg-purple-700' : 'hover:bg-purple-700'}`}
+                onClick={() => handleClick(2)}
+              >
+                Historico Reservas
+              </li>
+              <li
+                className={`p-4 cursor-pointer ${opcionSeleccionada === 3 ? 'bg-purple-700' : 'hover:bg-purple-700'}`}
+                onClick={() => handleClick(3)}
+              >
+                Reservas Activas
+              </li>
+            </ul>
+          </div>
+          <div className="flex-1">
+            {opcionSeleccionada === 2 && <HistoricoReservas />} {/* Mostrar HistoricoReservas solo si está seleccionada la opción 2 */}
+          </div>
         </div>
-      </div>
-      
-      <Footer /> {/* Incluye el Footer */}
+      </main>
+
+      <Footer />
       <button onClick={scrollToTop} className="fixed bottom-5 right-5 bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full shadow-lg z-50">
         Ir al Inicio
       </button>
@@ -39,5 +69,3 @@ const Historico = () => {
 };
 
 export default Historico;
-
-
